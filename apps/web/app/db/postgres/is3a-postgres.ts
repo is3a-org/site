44c240897a887cd97492ff05e032b5169cebac27
Lorian Coltof <loriancoltof@gmail.com>
2025-11-01 13:18:15 +0100

    Align DB migrations with DB schema and fix some other DB-related config

--- apps/web/app/db/postgres/is3a-postgres.ts
+++ apps/web/app/db/postgres/is3a-postgres.ts
@@ -6,14 +6,11 @@ import { Client } from "pg";
 export function createPostgresClient() {
   return new Client({
     connectionString: process.env.PG_DATABASE_URL!,
-    // query_timeout: 1000,
-    // statement_timeout: 1000,
-    // connectionTimeoutMillis: 1000,
   });
 }
 
 export function createDrizzleDatabase(client: Client) {
-  return drizzle({ client, schema, casing: "snake_case" });
+  return drizzle({ client, schema });
 }
 
 export type DrizzleDatabase = ReturnType<typeof createDrizzleDatabase>;
--- apps/web/app/db/postgres/migrations/0001_rename-tables.sql
+++ apps/web/app/db/postgres/migrations/0001_rename-tables.sql
@@ -1,3 +1,4 @@
 -- Custom SQL migration file, put your code below! --
 ALTER TABLE "session_simple-auth-db" RENAME TO "session_simple_auth_db";
 ALTER TABLE "user_simple-auth-db" RENAME TO "user_simple_auth_db";
+ALTER TABLE "session_simple_auth_db" ALTER COLUMN "userId" TYPE bigint USING ("userId"::bigint);
